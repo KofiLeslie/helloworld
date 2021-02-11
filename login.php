2fe7b7e45d58ba040a1cba2265c00c3226a94d91
KofiLeslie <51799467+KofiLeslie@users.noreply.github.com>
2021-02-11 11:59:30 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+	<title>My Chat</title>
+	<link rel="stylesheet" href="asset/css/login.css">
+</head>
+
+<body>
+
+	<div id="wrapper">
+ 
+ 		<div id="header">
+ 			My Chat
+ 			<div style="font-size: 20px;font-family: myFont;">Login</div>
+ 		</div>
+ 		<div id="error">some text</div>
+		<form id="myform">
+ 			<input type="text" name="email" placeholder="Email"><br>
+			<input type="password" name="password" placeholder="Password"><br>
+ 			<input type="submit" value="Login" id="login_button" ><br>
+
+ 			<br>
+ 			<a href="signup.php" style="display: block;text-align: center;text-decoration: none">
+ 				Don''t have an Account? Signup here
+ 			</a>
+		</form>
+	</div>
+	<script src="asset/js/login.js"></script>
+</body>
+</html>
+
+
